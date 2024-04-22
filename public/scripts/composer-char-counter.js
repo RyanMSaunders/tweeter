eec63af85fa52624457b065cf0a1b8e51bd7a984
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-04-22 14:15:53 +0000

    Lint and delete commented code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,18 @@
 
 $(document).ready(function() {
-  // --- our code goes here ---
+  
 
-  $(".text-area").on( "input", function() {
+  $(".text-area").on("input", function() {
     const $textarea = $(this).find('textarea');
-    const textLength = $textarea.val().length
-
-    // console.log(textLength);
-
+    const textLength = $textarea.val().length;
     const remainingTextLength = 140 - textLength;
-    // console.log(remainingTextLength);
-
     const $counter = $(this).siblings('.submit-tweet').find('.counter');
-    // console.log($textarea);
-    // console.log($counter);
-    $counter.html(remainingTextLength)
+    $counter.html(remainingTextLength);
 
     if (remainingTextLength < 0) {
       $counter.addClass('counter-negative');
     } else {
-      $counter.removeClass('counter-negative')
+      $counter.removeClass('counter-negative');
     }
 
   });
